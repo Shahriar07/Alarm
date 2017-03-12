d2ff5618ac8011edd54a0ed37a69a8b0c701051e
H. M. Shahriar <mshahriar.h@gmail.com>
2017-03-12 12:04:59 +0600

    First Commit

---
+++ mobile/src/main/java/com/shahriar/hasan/officealarm/AlarmDialogActivity.java
@@ -0,0 +1,60 @@
+package com.shahriar.hasan.officealarm;
+
+import android.app.Activity;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by USER on 3/11/2017.
+ */
+
+public class AlarmDialogActivity extends Activity {
+
+    Ringtone ringtone;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("AlarmDialogActivity", "ringtone Set " + ringtone);
+        setContentView(R.layout.alarm_dialog_activity);
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        ringtone = RingtoneManager.getRingtone(this.getApplicationContext(), alarmUri);
+        Log.d("AlarmActivity", "ringtone Set " + ringtone);
+        ringtone.play();
+//            setAlarmText("Alarm Set!! ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    public void onStopClicked (View view){
+//        Intent alarmIntent = new Intent(AlarmActivity.this, AlarmReceiver.class);
+//        pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this, 1911, alarmIntent, 0);
+//        Log.d("AlarmActivity", "Alarm stopped " + pendingIntent);
+//        alarmManager.cancel(pendingIntent);
+//        setAlarmText("Alarm Stopped!!");
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        // ringtone = RingtoneManager.getRingtone(this.getApplicationContext(), alarmUri);
+        Log.d("AlarmActivity", "ringtone Set " + ringtone);
+        ringtone.stop();
+        finish();
+    }
+
+}
